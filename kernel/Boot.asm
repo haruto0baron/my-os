3d9ea57ccf88d6e5081ddad9694106b06af7531d
haruto0baron <haru_to0baron@outlook.jp>
2026-05-14 20:34:23 +0900

    Add initial bootloader assembly code

---
+++ kernel/Boot.asm
@@ -0,0 +1,19 @@
+BITS 32
+
+section .multiboot
+align 4
+dd 0x1BADB002
+dd 0x00
+dd -(0x1BADB002 + 0x00)
+
+section .text
+global start
+extern kernel_main
+
+start:
+    cli
+    call kernel_main
+
+.hang:
+    hlt
+    jmp .hang
