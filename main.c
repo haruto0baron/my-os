f9abf6a61bd08fc23a654c143f74acec387d0d63
haruto0baron <haru_to0baron@outlook.jp>
2026-05-15 18:53:37 +0900

    Implement basic kernel main function

---
+++ main.c
@@ -0,0 +1,17 @@
+void kernel_main(void)
+{
+    volatile char* vga = (volatile char*)0xB8000;
+
+    const char* msg = "Hello MyOS!";
+
+    for (int i = 0; msg[i] != '\0'; i++) {
+        vga[i * 2] = msg[i];
+        vga[i * 2 + 1] = 0x0F;
+    }
+
+    __asm__ volatile ("cli");
+
+    while (1) {
+        __asm__ volatile ("hlt");
+    }
+}
